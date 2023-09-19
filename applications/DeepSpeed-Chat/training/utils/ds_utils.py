7b164509f361888b62b9e6df6c4b75f11766da6a
mlin <linming84@126.com>
2023-09-19 11:10:19 +0800

    Support both ZeRO stage 3 and stage 2

--- applications/DeepSpeed-Chat/training/utils/ds_utils.py
+++ applications/DeepSpeed-Chat/training/utils/ds_utils.py
@@ -24,41 +24,45 @@ def get_train_ds_config(offload,
                         tb_name=""):
 
     device = "cpu" if offload else "none"
-    # zero
-    """
-    zero_opt_dict = {
-        "stage": stage,
-        "offload_optimizer": {
-            "device": device
-        },
-        "offload_param": {
-            "device": device
-        },
-        "overlap_comm": False,
-        "contiguous_gradients": True,
-        "sub_group_size": 5e8,
-        "reduce_bucket_size": "auto",
-        "stage3_prefetch_bucket_size": "auto",
-        "stage3_param_persistence_threshold": "auto",
-        "stage3_max_live_parameters": 5e8,
-        "stage3_max_reuse_distance": 5e8,
-        "stage3_gather_16bit_weights_on_model_save": True,
-        "memory_efficient_linear": False
-    }
-    """
-    zero_opt_dict = {
-        "stage": stage,
-        "offload_optimizer": {
-            "device": device,
-            "pin_memory": True
-        },
-        "allgather_partitions": True,
-        "allgather_bucket_size": 5e8,
-        "reduce_scatter": True,
-        "reduce_bucket_size": 5e8,
-        "overlap_comm": False,
-        "contiguous_gradients": True
-    }
+    # zero configuration
+    if stage == 3:
+        zero_opt_dict = {
+            "stage": stage,
+            "offload_optimizer": {
+                "device": device
+            },
+            "offload_param": {
+                "device": device
+            },
+            "overlap_comm": False,
+            "contiguous_gradients": True,
+            "sub_group_size": 5e8,
+            "reduce_bucket_size": "auto",
+            "stage3_prefetch_bucket_size": "auto",
+            "stage3_param_persistence_threshold": "auto",
+            "stage3_max_live_parameters": 5e8,
+            "stage3_max_reuse_distance": 5e8,
+            "stage3_gather_16bit_weights_on_model_save": True,
+            "memory_efficient_linear": False
+        }
+    elif stage == 2:
+        zero_opt_dict = {
+            "stage": stage,
+            "offload_optimizer": {
+                "device": device,
+                "pin_memory": True
+            },
+            "allgather_partitions": True,
+            "allgather_bucket_size": 5e8,
+            "reduce_scatter": True,
+            "reduce_bucket_size": 5e8,
+            "overlap_comm": False,
+            "contiguous_gradients": True
+        }
+    else:
+        print("Unsupported stage - %d" % stage)
+        exit(-1)
+
     if enable_mixed_precision_lora:
         zero_opt_dict["zero_quantized_nontrainable_weights"] = True
         if dist.get_world_size() != torch.cuda.device_count():
@@ -93,24 +97,29 @@ def get_train_ds_config(offload,
 
 def get_eval_ds_config(offload, stage=0):
     device = "cpu" if offload else "none"
-    """
-    zero_opt_dict = {
-        "stage": stage,
-        "stage3_param_persistence_threshold": "auto",
-        "offload_param": {
-            "device": device
-        },
-        "memory_efficient_linear": False
-    }
-    """
-    zero_opt_dict = {
-        "stage": stage,
-        "offload_optimizer": {
-            "device": "cpu",
-            "pin_memory": True
-        },
-        "allgather_bucket_size": "auto"
-    }
+    # zero configuration
+    if stage == 3:
+        zero_opt_dict = {
+            "stage": stage,
+            "offload_optimizer": {
+                "device": "cpu",
+                "pin_memory": True
+            },
+            "allgather_bucket_size": "auto"
+        }
+    elif stage == 2:
+        zero_opt_dict = {
+            "stage": stage,
+            "stage3_param_persistence_threshold": "auto",
+            "offload_param": {
+                "device": device
+            },
+            "memory_efficient_linear": False
+        }
+    else:
+        print("Unsupported stage - %d" % stage)
+        exit(-1)
+
     return {
         "train_batch_size": "auto",
         "train_micro_batch_size_per_gpu": "auto",
