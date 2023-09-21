8d6aa6548b69a94ea310859e2e7f590db80b8a73
mlin <linming84@126.com>
2023-09-21 16:59:30 +0800

    Change step 2 & 3 scripts for correct actor and critic model configuration

--- applications/DeepSpeed-Chat/training/step2_reward_model_finetuning/training_scripts/llama2/run_llama2_13b.sh
+++ applications/DeepSpeed-Chat/training/step2_reward_model_finetuning/training_scripts/llama2/run_llama2_13b.sh
@@ -3,20 +3,27 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # DeepSpeed Team
-DATA_PATH=/mnt/mlin/bloom/data/Dahoas/rm-static/data
+DATA_PATH=/mnt/ai2/bloom/data/Dahoas/rm-static/data
 
 MODEL_SIZE=13b
 
-LLAMA2_PATH=/mnt/mlin/llama-2
+LLAMA2_PATH=/mnt/ai2/llama-2
+
 MODEL_PATH=$LLAMA2_PATH/Llama-2-${MODEL_SIZE}-hf
 
 CHECKPOINT_DIR=$LLAMA2_PATH/checkpoints/rm_Llama-2-${MODEL_SIZE}
 OUTPUT=$LLAMA2_PATH/models/rm_raw-${MODEL_SIZE}
 
+# TODO: we can compare the performance results of PPO by using reward model trained on continued pretrained model or SFT model
+#MODEL_PATH=$LLAMA2_PATH/models/llama_pt
+
+#CHECKPOINT_DIR=$LLAMA2_PATH/checkpoints/rm_tb-${MODEL_SIZE}
+#OUTPUT=$LLAMA2_PATH/models/rm_tb-${MODEL_SIZE}
+
 ZERO_STAGE=2
 LR=1e-5
 
-deepspeed main.py \
+nohup sh -c "deepspeed --include localhost:0,1,2,3,4,5 --master_port 28900 main.py \
    --data_path $DATA_PATH \
    --data_split 2,4,4 \
    --model_name_or_path $MODEL_PATH \
@@ -38,4 +45,5 @@ deepspeed main.py \
    --steps_per_print 1 \
    --checkpoint_steps 50 \
    --checkpoint_dir $CHECKPOINT_DIR \
-   --output_dir $OUTPUT
+   --output_dir $OUTPUT > rm.log 2>&1" &
+
--- applications/DeepSpeed-Chat/training/step3_rlhf_finetuning/training_scripts/llama2/run_llama2_13b.sh
+++ applications/DeepSpeed-Chat/training/step3_rlhf_finetuning/training_scripts/llama2/run_llama2_13b.sh
@@ -3,28 +3,42 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # DeepSpeed Team
-DATA_PATH=/mnt/mlin/bloom/data/Dahoas/rm-static/data
+DATA_PATH=/mnt/ai2/bloom/data/Dahoas/rm-static/data
 
 MODEL_SIZE=13b
 
-ACTOR_MODEL_PATH=/mnt/ai2/llama-2/Llama-2-${MODEL_SIZE}-hf
-CRITIC_MODEL_PATH=/mnt/ai2/llama-2/checkpoints/rm_raw-${MODEL_SIZE}
+LLAMA2_PATH=/mnt/ai2/llama-2
+
+ACTOR_MODEL_PATH=$LLAMA2_PATH/Llama-2-${MODEL_SIZE}-hf
+CRITIC_MODEL_PATH=$LLAMA2_PATH/checkpoints/rm_raw-${MODEL_SIZE}
+
+CHECKPOINT_DIR=$LLAMA2_PATH/checkpoints/rl_raw-${MODEL_SIZE}
+OUTPUT=$LLAMA2_PATH/models/rl_raw-${MODEL_SIZE}
+
+# actor model should use SFT model on continued pretrained model
+# use meta Llama 2 13b chat for testing currently, since llama_mt can not be loaded using from_pretrained directly
+#ACTOR_MODEL_PATH=$LLAMA2_PATH/models/llama_mt
+#ACTOR_MODEL_PATH=$LLAMA2_PATH/Llama-2-13b-chat-hf
+# critic model use reward model trained on continued pretrained model or SFT model
+#CRITIC_MODEL_PATH=$LLAMA2_PATH/checkpoints/rm_tb-${MODEL_SIZE}
+
+#CHECKPOINT_DIR=$LLAMA2_PATH/checkpoints/rl_tb-${MODEL_SIZE}
+#OUTPUT=$LLAMA2_PATH/models/rl_tb-${MODEL_SIZE}
 
 ACTOR_ZERO_STAGE=3
-CRITIC_ZERO_STAGE=2
+CRITIC_ZERO_STAGE=3
 
 Actor_Lr=9.65e-6
 Critic_Lr=5e-6
 
-OUTPUT=/mnt/mlin/llama-2/models/rl_tb-13b
 
-deepspeed --include localhost:0,1 --master_port 28579 main.py \
+nohup sh -c "deepspeed --include localhost:0,1,2,3,4,5,6,7 --master_port 28579 main.py \
    --data_path $DATA_PATH \
    --data_split 2,4,4 \
    --actor_model_name_or_path $ACTOR_MODEL_PATH \
    --critic_model_name_or_path $CRITIC_MODEL_PATH \
    --per_device_generation_batch_size 1 \
-   --per_device_training_batch_size 16 \
+   --per_device_training_batch_size 4 \
    --generation_batches 1 \
    --ppo_epochs 1 \
    --max_answer_seq_len 256 \
@@ -47,4 +61,4 @@ deepspeed --include localhost:0,1 --master_port 28579 main.py \
    --actor_zero_stage $ACTOR_ZERO_STAGE \
    --critic_zero_stage $CRITIC_ZERO_STAGE \
    --enable_hybrid_engine \
-   --output_dir $OUTPUT
+   --output_dir $OUTPUT > rl.log 2>&1" &
